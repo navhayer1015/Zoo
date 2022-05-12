32192ac395715f80e802b416db0b74b03f7d3045
LAPTOP-SUVUOO76\nav.hayer <nav.hayer@unity3d.com>
2022-05-12 15:24:22 -0400

    Moved all animal and enviroment logic into a single biome script moved the animals into the resouces and load everything from there. this will make it easier to update things not need to drag onto game objcet have the game objcet update the data before you go into play

--- Assets/Scripts/AnimalManager.cs
+++ Assets/Scripts/AnimalManager.cs
@@ -7,58 +7,5 @@ using UnityEngine;
 
 public class AnimalManager : MonoBehaviour
 {
-    [SerializeField] GameObject _currentAnimal;
-    [SerializeField] private int _currentAmimalIndex = 0;
-    [SerializeField] List<GameObject> _animals;
-    
-    [Button]
-    public void SpawnAnimal()
-    {
-        _currentAnimal =  Instantiate(_animals[_currentAmimalIndex],Vector3.zero, Quaternion.identity);
-        var boxCollider = _currentAnimal.GetComponentInChildren<BoxCollider>();
-        var animalPos = _currentAnimal.transform.position;
-        animalPos = new Vector3(animalPos.x, animalPos.y + math.abs(boxCollider.bounds.size.y), animalPos.z);
-        _currentAnimal.transform.position = animalPos;
-    }
-    [Button]
-    public void Next()
-    {
-        DestroyAnimal();
-        _currentAmimalIndex++;
-        if (_currentAmimalIndex <= _animals.Count - 1)
-        {
-            _currentAnimal = _animals[_currentAmimalIndex];
-        }
-        else
-        {
-            _currentAmimalIndex = 0;
-            _currentAnimal = _animals[_currentAmimalIndex];
-        }
 
-        SpawnAnimal();
-    }
-    [Button]
-    public void Previous()
-    {
-        DestroyAnimal();
-        _currentAmimalIndex--;
-        if (_currentAmimalIndex <= -1)
-        {
-            _currentAmimalIndex = _animals.Count - 1;
-            _currentAnimal = _animals[_currentAmimalIndex];
-        }
-        else
-        {
-            _currentAnimal = _animals[_currentAmimalIndex];
-        }
-        SpawnAnimal();
-    }
-    
-    // Destroy the current environment
-    void DestroyAnimal() 
-    { 
-        DestroyImmediate(_currentAnimal);
-    }
-    
-    
 }
--- Assets/Scripts/EnvironmentManager.cs
+++ Assets/Scripts/EnvironmentManager.cs
@@ -6,56 +6,6 @@ using UnityEngine;
 
 public class EnvironmentManager : MonoBehaviour
 {
-    [SerializeField] GameObject _currentEnvironment;
-    [SerializeField] private int _currentEnvironmentsIndex = 0;
-    [SerializeField] List<GameObject>  _environments;
 
     
-    [Button]
-    public void SpawnEnvironment()
-    {
-        _currentEnvironment = Instantiate(_environments[_currentEnvironmentsIndex],Vector3.zero, Quaternion.identity);
-        _currentEnvironment.transform.localScale = new Vector3(10,10,10);
-        Camera.main.GetComponent<CameraOrbit>().target = _currentEnvironment;
-    }
-    
-    [Button]
-    public void Next()
-    {
-        DestroyEnvironment();
-        _currentEnvironmentsIndex++;
-        if (_currentEnvironmentsIndex <= _environments.Count - 1)
-        {
-            _currentEnvironment = _environments[_currentEnvironmentsIndex];
-        }
-        else
-        {
-            _currentEnvironmentsIndex = 0;
-            _currentEnvironment = _environments[_currentEnvironmentsIndex];
-        }
-
-        SpawnEnvironment();
-    }
-    [Button]
-    public void Previous()
-    {
-        DestroyEnvironment();
-        _currentEnvironmentsIndex--;
-        if (_currentEnvironmentsIndex <= -1)
-        {
-            _currentEnvironmentsIndex = _environments.Count - 1;
-            _currentEnvironment = _environments[_currentEnvironmentsIndex];
-        }
-        else
-        {
-            _currentEnvironment = _environments[_currentEnvironmentsIndex];
-        }
-        SpawnEnvironment();
-    }
-    
-    // Destroy the current environment
-    void DestroyEnvironment() 
-    { 
-        DestroyImmediate(_currentEnvironment);
-    }
 }
