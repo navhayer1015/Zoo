3dccb48a1890a966f3f95e2843a812b5f0b12f68
Vicky Voinova <vicky.voinova@unity3d.com>
2022-05-13 14:30:17 -0400

    Restructured the UXML a bit; wrapped up initialization logic for UI controls

--- Assets/Scripts/Biome/BiomeView.cs
+++ Assets/Scripts/Biome/BiomeView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UIElements;
@@ -19,20 +20,26 @@ public class BiomeView : MonoBehaviour
     Label _size;
     Label _diet;
     Label _scientificName;
+    Label _shortFact;
 
     void Awake()
     {
         _uiDocument = GetComponent<UIDocument>();
+    }
+
+    void OnEnable()
+    {
         _uiDocument.rootVisualElement.Q<Button>("btnBack").clicked += OnBackButtonPressed;
         _uiDocument.rootVisualElement.Q<Button>("btnNext").clicked += OnNextPressed;
         _uiDocument.rootVisualElement.Q<Button>("btnPrevious").clicked += OnPreviousPressed;
 
         _animalName = _uiDocument.rootVisualElement.Q<Label>("lblName");
-    }
-
-    void OnEnable()
-    {
-        _biome.SpawnAnimal();
+        _scientificName = _uiDocument.rootVisualElement.Q<Label>("lblScientificName");
+        _shortFact = _uiDocument.rootVisualElement.Q<Label>("lblFact");
+        _habitat = _uiDocument.rootVisualElement.Q<Label>("lblHabitatValue");
+        _size = _uiDocument.rootVisualElement.Q<Label>("lblSizeValue");
+        _diet = _uiDocument.rootVisualElement.Q<Label>("lblDietValue");
+        
         InitForBiome(_biome);
     }
 
@@ -56,6 +63,11 @@ public class BiomeView : MonoBehaviour
     void InitializeAnimalData(AnimalData animalData)
     {
         _animalName.text = animalData._animalName;
+        _scientificName.text = animalData._scientificNames?.FirstOrDefault();
+        _shortFact.text = animalData._shortFact;
+        _diet.text = animalData._diet;
+        _habitat.text = animalData._habitat;
+        _size.text = animalData._size;
     }
 
     void OnNextPressed()
