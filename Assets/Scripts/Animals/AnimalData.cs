bce985c257da4cf73f26ad6b1ca094c0f84b8dcf
LAPTOP-SUVUOO76\nav.hayer <nav.hayer@unity3d.com>
2022-05-13 09:45:06 -0400

    added animaldata script to all prefab (not data has been filled out yet) moved script into folder for better organization

---
+++ Assets/Scripts/Animals/AnimalData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalData : MonoBehaviour
+{
+    public string _animalName;
+    public List<string> _scientificNames;
+    public string _shortFact;
+    public string _diet;
+    public string _habitat;
+    public string _size;
+}
